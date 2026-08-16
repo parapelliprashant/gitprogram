bf386fa221c23a8e297d8225a8ee506189c2dc44
Abhimishra2907 <mishraabhishek2907@gmail.com>
2026-08-16 12:11:47 +0530

    entity feild add

--- src/main/java/com/examplegitdemo/entities/User.java
+++ src/main/java/com/examplegitdemo/entities/User.java
@@ -1,4 +1,11 @@
 package com.examplegitdemo.entities;
 
+
 public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private Double Salary;
+
 }
